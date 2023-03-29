5f6f40f24f5ee89746de38ed192dd14c6ac8abf2
Daniela Castro <someonealiveh@gmail.com>
2023-03-29 19:47:08 +0100

    Converted time on each timer to seconds with two decimal places

--- Tempo/Program.cs
+++ Tempo/Program.cs
@@ -16,6 +16,14 @@ namespace Tempo
             Thread.Sleep(500);
             chrono2.Start();
             Thread.Sleep(300);
+            chrono1.Stop();
+            chrono2.Stop();
+
+            double chrono1String = chrono1.ElapsedMilliseconds / 1000.0;
+            double chrono2String = chrono2.ElapsedMilliseconds / 1000.0;
+            
+            Console.WriteLine($"First timer => {Math.Round(chrono1String, 2)} seconds");
+            Console.WriteLine($"Second timer => {Math.Round(chrono2String, 2)} seconds");
         }
     }
 }
