0be530b849200893a28481f133129c0bb3c24f42
Daniela Castro <someonealiveh@gmail.com>
2023-03-29 20:00:35 +0100

    Asked user for input

---
+++ RandomDice/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RandomDice
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Please input how many 1d6 dices you want to throw:");
+            int n = int.Parse(Console.ReadLine()!);
+
+        }
+    }
+}
