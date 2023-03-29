3c3b4316beef93d9cae7cb9348152dec42c2c5d4
Daniela Castro <someonealiveh@gmail.com>
2023-03-29 20:04:38 +0100

    Made a for loop to simulate the rolling of the dices

--- RandomDice/Program.cs
+++ RandomDice/Program.cs
@@ -9,6 +9,19 @@ namespace RandomDice
             Console.WriteLine("Please input how many 1d6 dices you want to throw:");
             int n = int.Parse(Console.ReadLine()!);
 
+            int soma = 0;
+
+            Random rando = new Random();
+
+            for (int i = 1; i <= n; i++)
+            {
+                int nextRoll = rando.Next(1, 7);
+                soma += nextRoll;
+                Console.WriteLine($"You've rolled {nextRoll} for die number {i}.");
+            }
+            
+            
+
         }
     }
 }
