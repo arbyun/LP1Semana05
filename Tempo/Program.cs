3bdcaedc38623ffb1c5912d16e3189b7ab6a63b0
Daniela Castro <someonealiveh@gmail.com>
2023-03-29 19:38:20 +0100

    Declared & Instantiated chrono1 and chrono2 and started counting time in chrono1

---
+++ Tempo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Tempo
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Type of var => Stopwatch
+            Stopwatch chrono1 = Stopwatch.StartNew();
+            Stopwatch chrono2 = Stopwatch.StartNew();
+            
+            chrono1.Start();
+        }
+    }
+}
