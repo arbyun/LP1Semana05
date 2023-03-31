54c5d3879de3cd4ca613294e288ed34b0d458090
Daniela Castro <someonealiveh@gmail.com>
2023-03-31 10:41:03 +0100

    Added UpperUnion to the solution and checked to see if arguments were passed

---
+++ UpperUnion/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace UpperUnion
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            // First we will check how many args were passed
+
+            if (args.Length == 0)
+            {
+                Console.WriteLine("NO ARGUMENTS");
+                return;
+            }
+            else
+            {
+                
+            }
+            
+        }
+    }
+}
